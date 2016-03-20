c6a61ac47333da26f499ce8b7c6006f28469f8f7
Andrey Shelkovnikov <shelkov09@gmail.com>
2016-03-21 01:38:59 +0200

    layout preparation, superhero theme

--- src/containers/GameBoard/GameBoard.js
+++ src/containers/GameBoard/GameBoard.js
@@ -1,20 +1,45 @@
-/* @flow */
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 // components
 import Field from './Field/Field'
-import Controls from './Controls/Controls'
+import ControlsPanel from './ControlsPanel/ControlsPanel'
 // stylization
 import classes from './GameBoard.scss'
 import classnames from 'classnames/bind'
 
 let cx = classnames.bind(classes)
 
+@connect((store) => ({palette: store.settings.palette}))
 export default class GameBoard extends React.Component {
+  static propTypes = {
+    palette: PropTypes.array.isRequired
+  };
+
   render () {
+    const { palette } = this.props
     return (
-      <div className={cx('game-container')}>
-        <Field/>
-        <Controls/>
+      <div>
+        <div className={cx('main')}>
+          <Field />
+          <ControlsPanel palette={palette} />
+        </div>
+        <div className={cx('left')}>
+          <div className='panel panel-default'>
+            <div className='panel-body'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque aut autem cumque deserunt
+                dolorem ea eaque eos error fugiat magnam molestiae molestias obcaecati placeat provident quaerat,
+                quia quis quisquam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque aut autem cumque deserunt
+                dolorem ea eaque eos error fugiat magnam molestiae molestias obcaecati placeat provident quaerat,
+                quia quis quisquam.
+              </p>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
